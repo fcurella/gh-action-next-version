01e9bd0063a2cbe1f1d31444e5e1e69f2a0dde0f
fcurella <flavio.curella@gmail.com>
2020-09-28 12:40:45 -0500

    split output from bumbversion

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,4 @@
-INFO=`bump2version --dry-run --list ${INPUT_PART}`
+INFO=`bump2version --dry-run --list ${INPUT_PART} | rs 0 1`
 
 CURRENT_VERSION=`echo ${INFO} | grep current_version | sed -r s,"^.*=",,`
 NEXT_VERSION=`echo ${INFO} | grep new_version | sed -r s,"^.*=",,`
