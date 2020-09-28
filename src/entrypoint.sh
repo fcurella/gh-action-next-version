c6b0e03c24ef2025245a727e94194b44feb5bcc2
fcurella <flavio.curella@gmail.com>
2020-09-28 11:53:03 -0500

    fix currentVersion output

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,5 +3,5 @@ INFO=`bump2version --dry-run --list ${INPUT_PART}`
 CURRENT_VERSION=`echo ${INFO} | grep current_version | sed -r s,"^.*=",,`
 NEXT_VERSION=`echo ${INFO} | grep new_version | sed -r s,"^.*=",,`
 
-echo ":set-output name=currentVersion::v${NEXT_VERSION}"
+echo ":set-output name=currentVersion::v${CURRENT_VERSION}"
 echo ":set-output name=nextVersion::v${NEXT_VERSION}"
